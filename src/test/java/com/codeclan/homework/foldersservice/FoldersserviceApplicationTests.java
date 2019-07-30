df1fd659a2dc656dd8d6d07bea7fbec2d0a65b93
Adamsh6 <adam.s.houten@gmail.com>
2019-07-30 14:55:51 +0100

    first commit

---
+++ src/test/java/com/codeclan/homework/foldersservice/FoldersserviceApplicationTests.java
@@ -0,0 +1,36 @@
+package com.codeclan.homework.foldersservice;
+
+import com.codeclan.homework.foldersservice.models.Folder;
+import com.codeclan.homework.foldersservice.models.User;
+import com.codeclan.homework.foldersservice.repositories.FolderRepository;
+import com.codeclan.homework.foldersservice.repositories.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoldersserviceApplicationTests {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	FolderRepository folderRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canCreateUser(){
+		User user = new User("Gerald");
+		userRepository.save(user);
+
+		Folder folder = new Folder("Photos", user);
+		folderRepository.save(folder);
+	}
+
+}
